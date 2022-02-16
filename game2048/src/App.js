e2ae4aa73370fae30c8b9264bbe46a8c2411f225
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-02-16 21:10:21 +0900

    기본구조 가져오기

--- game2048/src/App.js
+++ game2048/src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import AboveGame from "./component/AboveGame";
+import Game from "./component/Game";
+import Header from "./component/Header";
 
 export default function App() {
-  return <div className="App">adf</div>;
+  return (
+    <div className="container">
+      <Header />
+      <AboveGame />
+      <Game />
+    </div>
+  );
 }
